262ab04e0a0e12c118e0fef4527506ccacf3e823
johansmarius <jsmarius@hotmail.com>
2025-11-18 10:45:28 +0100

    new build

--- EventsAppp/obj/Debug/net10.0-ios/iossimulator-arm64/EventsAppp.AssemblyInfo.cs
+++ EventsAppp/obj/Debug/net10.0-ios/iossimulator-arm64/EventsAppp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsAppp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+322268a92eb92194cf60fa1c440563be2440d2cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d85b162ea8dd16c068495cdf504a58af00b9f2f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsAppp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsAppp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
